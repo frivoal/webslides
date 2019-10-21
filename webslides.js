cfa66ce57587627550867f6b1bb810cc0226615d
Florian Rivoal <git@florian.rivoal.net>
2019-10-21 17:56:16 +0900

    Update Copyright

--- webslides.js
+++ webslides.js
@@ -1,6 +1,6 @@
 /* MIT License
 
-Copyright 2018 Florian Rivoal
+Copyright 2018–2019 Florian Rivoal
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
